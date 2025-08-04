21e987f9c6a195d89258130d253368d18a27d07f
Sanitta_Johnson <sanittajohnson28@gmail.com>
2025-08-04 09:28:58 +0530

    Time: 4 ms (14.97%) | Memory: 56.1 MB (87.94%) - LeetSync

---
+++ 3778-transform-array-by-parity/transform-array-by-parity.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var transformArray = function(nums) {
+ 
+   
+    for(i=0;i<nums.length;i++){
+        if(nums[i]%2===0){
+            nums[i]=0
+        }else{
+            nums[i]=1
+        }
+    }
+    return   nums.sort((a,b)=>a-b)
+};
