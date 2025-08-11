4be7fa72a2fb33046cab28f52e9a3e11cbf8aa37
Sanitta_Johnson <sanittajohnson28@gmail.com>
2025-08-11 16:40:20 +0530

    Time: 2 ms (44.44%) | Memory: 56.6 MB (78.67%) - LeetSync

--- 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.js
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.js
@@ -4,6 +4,6 @@
  * @return {number}
  */
 var minOperations = function(nums, k) {
-    const sum=nums.reduce((acc,cur)=>acc+cur,0)
+    let sum=nums.reduce((acc,cur)=>acc+cur,0)
     return sum%k
 };
