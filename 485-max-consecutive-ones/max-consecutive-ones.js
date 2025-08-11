89b407f5c47c7fc200f3b623c6889fdec5b913d1
Sanitta_Johnson <sanittajohnson28@gmail.com>
2025-08-11 15:38:41 +0530

    Time: 0 ms (100.00%) | Memory: 60.2 MB (47.64%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let maxCount=0
+    let count=0
+    for(i=0;i<nums.length;i++){
+        if(nums[i]===1){
+            count++
+            maxCount=Math.max(maxCount,count)
+        }else{
+            count=0
+        }
+    }
+    return maxCount
+};
