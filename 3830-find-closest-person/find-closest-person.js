1ca39822f59cc582d594a0f83d355033752dac43
Sanitta_Johnson <sanittajohnson28@gmail.com>
2025-09-09 09:34:14 +0530

    Time: 16 ms (1.99%) | Memory: 57.8 MB (5.52%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {number}
+ */
+var findClosest = function(x, y, z) {
+    let a=Math.abs(x-z)
+    let b=Math.abs(y-z)
+    console.log(a)
+     console.log(b)
+    if(a===b) return 0
+
+    return a<b ? 1:2
+
+};
