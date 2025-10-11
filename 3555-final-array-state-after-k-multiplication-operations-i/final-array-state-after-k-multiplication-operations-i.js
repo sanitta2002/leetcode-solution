e04509f98a415997a84d18b879039ab55f8677c6
Sanitta_Johnson <sanittajohnson28@gmail.com>
2025-10-11 19:36:23 +0530

    Time: 4 ms (18.97%) | Memory: 58.1 MB (25.00%) - LeetSync

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} multiplier
+ * @return {number[]}
+ */
+var getFinalState = function(nums, k, multiplier) {
+    for(i=0;i<k;i++){
+        let index=0
+        for(j=1;j<nums.length;j++){
+            if(nums[j]<nums[index]){
+                index=j
+            }
+        }
+        nums[index]*=multiplier
+    }
+    return nums
+};
