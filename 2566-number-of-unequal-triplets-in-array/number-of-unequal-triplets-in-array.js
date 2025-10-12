e8c30e89525c05ed30f45b3293acb3b23636285c
Sanitta_Johnson <sanittajohnson28@gmail.com>
2025-10-12 15:05:07 +0530

    Time: 12 ms (14.49%) | Memory: 54.4 MB (69.57%) - LeetSync

---
+++ 2566-number-of-unequal-triplets-in-array/number-of-unequal-triplets-in-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var unequalTriplets = function(nums) {
+    let count=0
+    for(i=0;i<nums.length;i++){
+        for(j=i+1;j<nums.length;j++){
+           for(k=j+1;k<nums.length;k++){
+            if(nums[i]!==nums[j] && nums[i]!==nums[k] && nums[j]!==nums[k]){
+               count++
+            }
+           }
+        }
+    }
+    return count
+};
