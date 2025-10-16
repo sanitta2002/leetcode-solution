7465ba31510572eb8390f62763f1b96b2c1cf6ec
Sanitta_Johnson <sanittajohnson28@gmail.com>
2025-10-16 17:18:54 +0530

    Time: 0 ms (100.00%) | Memory: 53.5 MB (89.04%) - LeetSync

---
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+    let str1=word1.join('')
+    let str2=word2.join('')
+    if(str1===str2){
+        return true
+    }
+    return false
+};
