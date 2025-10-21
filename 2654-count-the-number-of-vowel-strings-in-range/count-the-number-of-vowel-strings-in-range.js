46f7b5b8753cfb367fa63e71d2e0fb172af6fbdd
Sanitta_Johnson <sanittajohnson28@gmail.com>
2025-10-21 17:43:41 +0530

    Time: 1 ms (93.33%) | Memory: 58.5 MB (60.00%) - LeetSync

--- 2654-count-the-number-of-vowel-strings-in-range/count-the-number-of-vowel-strings-in-range.js
+++ 2654-count-the-number-of-vowel-strings-in-range/count-the-number-of-vowel-strings-in-range.js
@@ -4,18 +4,15 @@
  * @param {number} right
  * @return {number}
  */
-var vowelStrings = function (words, left, right) {
-    let count = 0
-    let w = ['a', 'e', 'i', 'o', 'u']
-    for (i = left; i <= right; i++) {
-
-        if (w.includes(words[i][0]) && w.includes(words[i][words[i].length - 1])) {
+var vowelStrings = function(words, left, right) {
+    let count=0
+    let w=['a','e','i','o','u']
+    for(i=left;i<=right;i++){
+        if(w.includes(words[i][0]) && w.includes(words[i][words[i].length-1])){
             count++
-
-
         }
-
     }
+      
+        return count
 
-    return count;
 };
