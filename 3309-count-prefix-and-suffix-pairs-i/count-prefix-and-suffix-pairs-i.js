d7b2a702f9e8b9364a66597ae82dd6c2e7615d22
Sanitta_Johnson <sanittajohnson28@gmail.com>
2025-10-22 20:06:21 +0530

    Time: 1 ms (96.20%) | Memory: 55.9 MB (64.56%) - LeetSync

---
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var countPrefixSuffixPairs = function(words) {
+    let count=0
+    for(i=0;i<words.length;i++){
+        for(j=i+1;j<words.length;j++){
+            if(words[j].startsWith(words[i]) && words[j].endsWith(words[i])){
+                count++
+            }
+        }
+    }
+    return count
+};
