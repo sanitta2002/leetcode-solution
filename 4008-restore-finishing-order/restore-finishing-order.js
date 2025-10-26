be2cf36faeaebfcb2bbbb8bb0d365b0fa8a211a4
Sanitta_Johnson <sanittajohnson28@gmail.com>
2025-10-26 17:50:08 +0530

    Time: 2 ms (46.11%) | Memory: 57.8 MB (64.72%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.js
+++ 4008-restore-finishing-order/restore-finishing-order.js
@@ -7,12 +7,10 @@ var recoverOrder = function(order, friends) {
     let arr=[]
     for(i=0;i<order.length;i++){
         for(j=0;j<friends.length;j++){
-            if(friends[j]===order[i]){
-                console.log(order[i])
+            if(order[i]===friends[j]){
                 arr.push(order[i])
             }
         }
-
     }
     return arr
 };
