894d3123c97fd73f302a5bf44ffd350c0f020040
Sanitta_Johnson <sanittajohnson28@gmail.com>
2025-11-28 12:17:10 +0530

    Time: 6 ms (93.07%) | Memory: 59 MB (55.94%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} score
+ * @return {string[]}
+ */
+var findRelativeRanks = function(score) {
+let res=[]
+let map={}
+ let sort= [...score].sort((a,b)=>b-a)
+  for(i=0;i<sort.length;i++){
+    if(i===0){
+        map[sort[i]]="Gold Medal"
+    }else if(i===1){
+        map[sort[i]]="Silver Medal"
+    }else if(i===2){
+         map[sort[i]]="Bronze Medal"
+    }else{
+        map[sort[i]]=(i+1).toString()
+    }
+  }
+  for(i=0;i<score.length;i++){
+    res[i]=map[score[i]]
+  }
+  return res
+};
