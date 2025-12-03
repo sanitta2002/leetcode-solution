866ac253c088b963d94ee9c48d0af7b08b410f23
Sanitta_Johnson <sanittajohnson28@gmail.com>
2025-12-03 09:33:33 +0530

    Time: 0 ms (100.00%) | Memory: 55.9 MB (38.32%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+var divide = function(dividend, divisor) {
+    if (dividend === -2147483648 && divisor === -1) {
+        return 2147483647;
+    }
+    let a=dividend/divisor
+     return Math.trunc(a)
+};
