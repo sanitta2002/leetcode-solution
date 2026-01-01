4c9e7f71c360e695eb47fdcff5235de6711c3c60
Sanitta_Johnson <sanittajohnson28@gmail.com>
2026-01-01 16:27:50 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (39.29%) - LeetSync

---
+++ 66-plus-one/plus-one.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+     for(var i=digits.length-1;i>=0;i--)
+  {
+     digits[i]++
+     if(digits[i]<10)
+     {
+        return digits
+     }
+    digits[i]=0
+  }
+
+  digits.unshift(1)
+   
+   return digits
+};
