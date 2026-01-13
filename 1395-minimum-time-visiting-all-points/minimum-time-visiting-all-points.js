71be4c92aef2ab48d4b762dbd6fe7cc67c2f8289
Sanitta_Johnson <sanittajohnson28@gmail.com>
2026-01-13 10:20:53 +0530

    Time: 1 ms (67.02%) | Memory: 54.2 MB (95.74%) - LeetSync

---
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var minTimeToVisitAllPoints = function(points) {
+    let time=0
+    for(let i=1;i<points.length;i++){
+        let x=Math.abs(points[i][0]-points[i-1][0])
+        let y=Math.abs(points[i][1]-points[i-1][1])
+        time+=Math.max(x,y)
+    }
+    return time
+};
