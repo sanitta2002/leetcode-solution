385845d01543a9abc78b305aa9ffb559fa866edd
Sanitta_Johnson <sanittajohnson28@gmail.com>
2026-02-18 10:46:20 +0530

    Time: 1 ms (65.17%) | Memory: 54.7 MB (22.70%) - LeetSync

--- 1137-height-checker/height-checker.js
+++ 1137-height-checker/height-checker.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var heightChecker = function(heights) {
-    let ex=[...heights].sort((a,b)=>a-b)
+    let expected=[...heights].sort((a,b)=>a-b)
     let count=0
-    for(i=0;i<heights.length;i++){
-        if(heights[i]!==ex[i]){
+    for(let i=0;i<heights.length;i++){
+          if(heights[i]!==expected[i]) {
             count++
-        }
+          }
     }
     return count
 };
