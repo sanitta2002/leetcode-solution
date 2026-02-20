7a2178a535c5231c6ac9639824defcc9cfe44927
Sanitta_Johnson <sanittajohnson28@gmail.com>
2026-02-20 09:59:08 +0530

    Time: 32 ms (29.56%) | Memory: 58.7 MB (64.52%) - LeetSync

--- 506-relative-ranks/relative-ranks.js
+++ 506-relative-ranks/relative-ranks.js
@@ -3,22 +3,20 @@
  * @return {string[]}
  */
 var findRelativeRanks = function(score) {
-let res=[]
-let map={}
- let sort= [...score].sort((a,b)=>b-a)
-  for(i=0;i<sort.length;i++){
-    if(i===0){
-        map[sort[i]]="Gold Medal"
-    }else if(i===1){
-        map[sort[i]]="Silver Medal"
-    }else if(i===2){
-         map[sort[i]]="Bronze Medal"
-    }else{
-        map[sort[i]]=(i+1).toString()
+    let arr=[]
+    let a=[...score].sort((a,b)=>b-a)
+    let res=new Array(score.length)
+    for(let i=0;i<a.length;i++){
+        let idx = score.indexOf(a[i])
+        if(i===0){
+            res[idx]="Gold Medal"
+        }else if(i===1){
+            res[idx]="Silver Medal"
+        }else if(i===2){
+             res[idx]="Bronze Medal"
+        }else{
+             res[idx]=(i+1).toString()
+        }
     }
-  }
-  for(i=0;i<score.length;i++){
-    res.push(map[score[i]])
-  }
-  return res
+   return res
 };
