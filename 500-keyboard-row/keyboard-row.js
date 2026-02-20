fd93564b181a8512f62946c10dab4f54d671ac99
Sanitta_Johnson <sanittajohnson28@gmail.com>
2026-02-20 09:39:45 +0530

    Time: 0 ms (100.00%) | Memory: 53.2 MB (84.64%) - LeetSync

--- 500-keyboard-row/keyboard-row.js
+++ 500-keyboard-row/keyboard-row.js
@@ -3,32 +3,30 @@
  * @return {string[]}
  */
 var findWords = function(words) {
-    let firstRow ='qwertyuiop'
-    let secRow  = 'asdfghjkl'
-    let thirdRow = 'zxcvbnm'
+    let  firstrow ="qwertyuiop"
+    let  secondrow ="asdfghjkl"
+    let  thirdrow ="zxcvbnm"
     let arr=[]
-
-    for(i=0;i<words.length;i++){
+    for(let i=0;i<words.length;i++){
         let a=words[i].toLowerCase()
         let row=''
-        if(firstRow.includes(a[0])){
-            row=firstRow
-        }else if(secRow.includes(a[0])){
-            row=secRow
+        if(firstrow.includes(a[0])){
+            row=firstrow
+        }else if(secondrow.includes(a[0])){
+            row=secondrow
         }else{
-            row=thirdRow
+            row=thirdrow
         }
-       
-       let sameRow=true
-       for(j=0;j<a.length;j++){
-          if(!row.includes(a[j])){
-            sameRow=false
-            break
-          }
-       }
-       if(sameRow){
-        arr.push(words[i])
-       }
+        let somerow=true
+        for(let j=0;j<a.length;j++){
+            if(!row.includes(a[j])){
+                somerow=false
+                break
+            }
+        }
+        if(somerow){
+            arr.push(words[i])
+        }
     }
     return arr
 };
