d3556d3915c8f5da329aac8fb6d7f52757e0be70
Sanitta_Johnson <sanittajohnson28@gmail.com>
2026-02-26 10:03:29 +0530

    Time: 2 ms (37.50%) | Memory: 55.9 MB (12.50%) - LeetSync

---
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numSteps = function(s) {
+    let num = BigInt('0b' + s);
+     let steps = 0n;
+
+    while(num > 1n){
+        if(num % 2n === 0n){
+            num = num / 2n;
+        }else{
+            num = num + 1n;
+        }
+        steps++;
+    }
+    return Number(steps);
+};
