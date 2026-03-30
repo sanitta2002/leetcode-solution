cb99225a837bf9f1c899bf778f953aaaecce9994
Sanitta_Johnson <sanittajohnson28@gmail.com>
2026-03-30 09:14:26 +0530

    Time: 220 ms (10.00%) | Memory: 73.9 MB (30.00%) - LeetSync

---
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkStrings = function(s1, s2) {
+     if(s1.length!==s2.length) return false
+    let even1=[]
+    let even2=[]
+    let odd1=[]
+    let odd2=[]
+
+    for(let i=0;i<s1.length;i++){
+        if(i%2===0){
+            even1.push(s1[i])
+            even2.push(s2[i])
+        }else{
+            odd1.push(s1[i])
+            odd2.push(s2[i])
+        }
+    }
+   even1.sort()
+   even2.sort()
+   odd1.sort()
+   odd2.sort()
+
+   return even1.join('')===even2.join('') && odd1.join('')===odd2.join('')
+};
