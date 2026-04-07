b2fc8cfbab32caa93b5e2ecbb4d7187c98df7bc2
Sanitta_Johnson <sanittajohnson28@gmail.com>
2026-04-07 09:33:25 +0530

    Time: 26 ms (57.23%) | Memory: 62.7 MB (29.65%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+    g.sort((a,b)=>a-b)
+    s.sort((a,b)=>a-b)
+    let count=0
+    let i=0;
+    let j=0;
+    while(i<g.length && j<s.length){
+        if(s[j]>=g[i]){
+            count++
+            i++
+            j++
+        }else{
+            j++
+        }
+    }
+    return count
+};
