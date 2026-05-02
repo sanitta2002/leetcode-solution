0bd1222ab9674cca68fab669ecbbe48ebd015fa8
Sanitta_Johnson <sanittajohnson28@gmail.com>
2026-05-02 10:45:57 +0530

    Time: 1 ms (62.71%) | Memory: 55.9 MB (35.59%) - LeetSync

--- 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.js
+++ 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 var minOperations = function(nums, k) {
-    let set=new Set()
+     let set=new Set()
     let count=0
     for(let i=nums.length-1;i>=0;i--){
         count++
