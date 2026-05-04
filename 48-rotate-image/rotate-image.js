a2822a5a1358968589e06544293fd6d9857cb894
Sanitta_Johnson <sanittajohnson28@gmail.com>
2026-05-04 09:36:46 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (67.16%) - LeetSync

--- 48-rotate-image/rotate-image.js
+++ 48-rotate-image/rotate-image.js
@@ -3,14 +3,15 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-   
-    for(i=0;i<matrix.length;i++){
-        for(j=i;j<matrix.length;j++){
-            [matrix[i][j],matrix[j][i]]=[matrix[j][i],matrix[i][j]]
+    for(let i=0;i<matrix.length;i++){
+        for(let j=i;j<matrix.length;j++){
+          let temp=matrix[i][j]
+          matrix[i][j]=matrix[j][i]
+          matrix[j][i]=temp
         }
     }
-    for(i=0;i<matrix.length;i++){
-        matrix[i].reverse()
-    }
+    for(let i=0;i<matrix.length;i++){
+            matrix[i].reverse()
+        }
     return matrix
 };
