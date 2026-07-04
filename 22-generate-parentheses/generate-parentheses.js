58883bae0fb8f443ca6325395eaaf605eae4aedf
Sanitta_Johnson <sanittajohnson28@gmail.com>
2026-07-04 11:01:29 +0530

    Time: 1 ms (75.58%) | Memory: 53.3 MB (91.13%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    let res=[]
+    function backtrack(current,open,close){
+        if(current.length===2*n){
+            res.push(current)
+            return
+        }
+        if(open<n){
+            backtrack(current+"(",open+1,close)
+        }
+        if(close<open){
+            backtrack(current+")",open,close+1)
+        }
+    }
+    backtrack("",0,0)
+    return res
+};
