989de592f30e2102e5e025140e66c32e36ce993b
Sanitta_Johnson <sanittajohnson28@gmail.com>
2026-07-10 09:31:37 +0530

    Time: 52 ms (38.73%) | Memory: 57.4 MB (56.96%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    let slow = head
+    let fast=head
+    while(fast !== null && fast.next !== null){
+        slow = slow.next;
+        fast = fast.next.next;
+          if(slow===fast) break
+    }
+     if (!fast || !fast.next) return null;
+
+     fast = head;
+    while (fast !== slow) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+
+    return slow;    
+};
