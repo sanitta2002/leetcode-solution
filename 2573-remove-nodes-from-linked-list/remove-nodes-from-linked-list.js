8c5d0e52eaa063f2bb59b3ebe7ece5cd0ea2c7f0
Sanitta_Johnson <sanittajohnson28@gmail.com>
2026-07-17 09:29:48 +0530

    Time: 13 ms (58.33%) | Memory: 101.4 MB (12.88%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var removeNodes = function(head) {
+     if (head === null || head.next === null) {
+        return head;
+    }
+
+    head.next = removeNodes(head.next);
+
+    if (head.next !== null && head.val < head.next.val) {
+        return head.next;
+    }
+
+    return head;
+};
