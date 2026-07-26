6adf3eb0d748a5411ce5d368d61cd3d1bef4b4df
Sanitta_Johnson <sanittajohnson28@gmail.com>
2026-07-26 21:25:57 +0530

    Time: 37 ms (46.88%) | Memory: 59 MB (54.46%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.js
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var maximumProduct = function(nums) {
-    nums.sort((a,b)=>a-b)
+       nums.sort((a,b)=>a-b)
     let n=nums.length
     let pro1=nums[n-1]*nums[n-2]*nums[n-3]
     let pro2=nums[n-1]*nums[0]*nums[1]
