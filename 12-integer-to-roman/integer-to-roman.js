e77706b7352feff1208e1609d88f6df753a5efe5
Sanitta_Johnson <sanittajohnson28@gmail.com>
2026-07-30 09:16:07 +0530

    Time: 7 ms (41.06%) | Memory: 64.1 MB (20.69%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    const r = [
+        [1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'],
+        [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'],
+        [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']
+    ];
+    let romen=''
+    for(let [value,s] of r){
+        while(num>=value){
+            romen+=s
+            num-=value
+        }
+
+    }
+    return romen
+};
