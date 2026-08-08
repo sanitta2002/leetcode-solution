b36a31359d28b6a5175658033002d0e089f71d12
Sanitta_Johnson <sanittajohnson28@gmail.com>
2026-08-08 09:44:58 +0530

    Time: 3 ms (98.31%) | Memory: 63.5 MB (98.02%) - LeetSync

---
+++ 463-island-perimeter/island-perimeter.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var islandPerimeter = function(grid) {
+    let a=0
+    for(let i=0;i<grid.length;i++){
+        for(let j=0;j<grid[0].length;j++){
+            if(grid[i][j]===1){
+                a+=4
+                if (i > 0 && grid[i - 1][j] === 1) {
+                    a -= 2;
+                }
+                if (j > 0 && grid[i][j - 1] === 1) {
+                    a-= 2;
+                }
+            }
+        }
+    }
+    return a
+};
