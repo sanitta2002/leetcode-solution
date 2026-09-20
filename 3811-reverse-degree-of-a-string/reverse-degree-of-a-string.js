f1a27c3d2e77ca654d451dcd768f48981e17cf09
Sanitta_Johnson <sanittajohnson28@gmail.com>
2026-09-20 12:39:51 +0530

    Time: 1 ms (81.18%) | Memory: 55.4 MB (84.71%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.js
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var reverseDegree = function(s) {
-    let res=0
+      let res=0
     for(let i=1;i<=s.length;i++){
         let char =s.charAt(i-1)
         let value = 26-(char.charCodeAt(0)-'a'.charCodeAt(0))
